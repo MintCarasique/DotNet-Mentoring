2df01bc62734edd0fd51683aa7c751d8b0db3ea9
Artem Slizhevskiy <mykew98@gmail.com>
2019-03-09 18:09:29 +0300

    Added FileSystemVisitor solution

---
+++ Advanced CSharp/FileSystemVisitor/FSV.Console/FSV.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FSV.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FSV.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6176ce81-97b5-46d6-aabc-3cc6ae75d357")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Advanced CSharp/FileSystemVisitor/FSV.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSV.Library
+{
+    public class Class1
+    {
+    }
+}
