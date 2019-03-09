5733af87c2ed1abd050781fac20ece3a3f68f52b
Artem Slizhevskiy <mykew98@gmail.com>
2019-03-09 17:44:26 +0300

    Added .NET Core hello app

---
+++ Introduction to .NET/HelloWorldCore/HelloWorldConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorldConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}!");
+        }
+    }
+}
