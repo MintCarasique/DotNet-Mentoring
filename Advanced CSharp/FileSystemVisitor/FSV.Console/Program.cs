dca1a6989f00ca8f795f943784402daa918903e0
Artem Slizhevskiy <mykew98@gmail.com>
2019-03-09 19:15:45 +0300

    Added test usage for FileSystemVisitor

--- Advanced CSharp/FileSystemVisitor/FSV.Console/Program.cs
+++ Advanced CSharp/FileSystemVisitor/FSV.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FSV.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,18 @@ namespace FSV.Console
 {
     class Program
     {
+        private const string testPath = "D:\\Documents\\DotNet-Mentoring\\";
+
         static void Main(string[] args)
         {
+            var fileProc = new FileSystemVisitor();
+
+            var fileEntries = fileProc.PerformProcess(testPath);
+            
+            foreach(var entry in fileEntries)
+            {
+                System.Console.WriteLine(entry);
+            }
         }
     }
 }
---
+++ Advanced CSharp/FileSystemVisitor/FSV.Library/FileSystemVisitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSV.Library
+{
+    public class FileSystemVisitor
+    {
+        public IEnumerable<string> PerformProcess(string startPath)
+        {
+            string[] entries = Directory.GetFileSystemEntries(startPath);
+
+            return entries;
+        }
+    }
+}
