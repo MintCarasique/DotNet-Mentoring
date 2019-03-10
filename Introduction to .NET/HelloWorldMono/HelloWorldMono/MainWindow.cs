ae07fb94dde95480cfafc4b8d4355318b86e3fe6
Artem Slizhevskiy <mykew98@gmail.com>
2019-03-11 00:10:51 +0300

    Added HelloWorld mono solution

---
+++ Introduction to .NET/HelloWorldMono/HelloWorldMono/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnApplyButtonClicked(object sender, EventArgs e)
+    {
+        var name = inputEntry.Text;
+        resultEntry.Text = $"Hello, {name}!";
+    }
+}
