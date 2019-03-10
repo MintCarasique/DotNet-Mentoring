84c3c66b9fe822ab4eb418ac54fcacdc6dda17b5
Artem Slizhevskiy <mykew98@gmail.com>
2019-03-11 00:49:40 +0300

    Added code for HelloWorld Android

--- Introduction to .NET/HelloWorldDroid/HelloWorldApp/HelloWorldApp/MainPage.xaml.cs
+++ Introduction to .NET/HelloWorldDroid/HelloWorldApp/HelloWorldApp/MainPage.xaml.cs
@@ -9,9 +9,34 @@ namespace HelloWorldApp
 {
     public partial class MainPage : ContentPage
     {
+        Label textLabel;
+        Entry nameEntry;
         public MainPage()
         {
-            InitializeComponent();
+            StackLayout stackLayout = new StackLayout();
+
+            nameEntry = new Entry { Placeholder = "Login" };
+            nameEntry.TextChanged += nameEntry_TextChanged;
+
+            textLabel = new Label { FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) };
+
+            stackLayout.Children.Add(nameEntry);
+            stackLayout.Children.Add(textLabel);
+
+            this.Content = stackLayout;
+        }
+
+        private void nameEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(nameEntry.Text.Length > 0)
+            {
+                textLabel.Text = $"Hello, {nameEntry.Text}!";
+            }
+            else
+            {
+                textLabel.Text = string.Empty;
+            }
+            
         }
     }
 }
