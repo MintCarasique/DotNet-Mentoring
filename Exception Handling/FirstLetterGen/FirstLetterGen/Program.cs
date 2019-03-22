34ab2d2090de12fe61f3ba3d7d19f2b7619469fc
Artsiom_Slizheuski <mykew98@gmail.com>
2019-03-22 18:52:53 +0300

    Added first task solution from 3rd module

---
+++ Exception Handling/FirstLetterGen/FirstLetterGen/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLetterGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
