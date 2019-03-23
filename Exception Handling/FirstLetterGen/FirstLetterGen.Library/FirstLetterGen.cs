65980a2fe55b8b889d7cf7f5229ac0e7dfc57b8b
Artsiom_Slizheuski <mykew98@gmail.com>
2019-03-23 19:56:43 +0300

    Added InvalidOperationException handling

--- Exception Handling/FirstLetterGen/FirstLetterGen.Library/FirstLetterGen.cs
+++ Exception Handling/FirstLetterGen/FirstLetterGen.Library/FirstLetterGen.cs
@@ -8,6 +8,20 @@ namespace FirstLetterGen.Library
 {
     public static class FirstLetterGen
     {
-        public static char ReturnFirstLetter(string input) => input.First();
+        public static string ReturnFirstLetter(string input)
+        {
+            try
+            {
+                if(input.First() == ' ')
+                {
+                    return "<SPACE>";
+                }
+                return input.First().ToString();
+            }
+            catch (InvalidOperationException)
+            {
+                return "<EMPTY STRING>";
+            }
+        }
     }
 }
