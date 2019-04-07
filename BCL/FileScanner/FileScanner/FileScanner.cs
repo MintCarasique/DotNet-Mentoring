bf9f996990097b09e26ebf10308622166a7bf2d4
Artsiom_Slizheuski <mykew98@gmail.com>
2019-04-07 22:12:49 +0300

    Added base class for scanner

---
+++ BCL/FileScanner/FileScanner/FileScanner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScanner
+{
+    public class FileScanner
+    {
+        public FileScanner(IEnumerable<string> directories)
+        {
+            foreach (var directory in directories)
+            {
+                
+            }
+        }
+
+        private void AddDirectoryToScan(string directory)
+        {
+
+        }
+    }
+}
